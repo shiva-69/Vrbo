9ba72c5a33d14f95c00265dfc9d028632d85016e
Shivam Pandey <shivam8kp@gmail.com>
2022-08-03 23:14:42 +0530

    fixed exposed files

--- Backend/Routers/getcity.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const getCity = require("../Handlers/getCity");
-
-const getCityRouter = express.Router()
-
-getCityRouter.get('/vrbo/:name', getCity)
-
-module.exports = getCityRouter;
